9c15409107083436744463fcb93cf85d4ccffb7f
Stefanos Triantafyllidis <stt0242@my.londonmet.ac.uk>
2021-10-05 21:02:58 +0100

    Access Violation errors on Music/Sound

--- SDLGameAssignment/Game.cpp
+++ SDLGameAssignment/Game.cpp
@@ -1,38 +1,36 @@
-#include "Game.h"
+#include "Game.h" 
 
 Game::Game()
 {
 	m_isGameRunning = true;
-	Screen screen; //Declaring Screen 	
-	screen.Init(); //Initialising screen
-	Text::Initialise(); //Implemented in Menu
-	Music::Initialize();
-
+	Screen screen; //Declaring Screen 	 
+	screen.Init(); //Initialising screen 
+	Text::Initialise(); //Implemented in Menu 
 	Background background(screen);
 	Bullet bullet(screen);
 	CowboyP2 cowboy(screen);
 	Player player(screen);
 	Input input;
-	//Score and Timer
+	//Score and Timer 
 	Score* score = new Score;
 	Timer* timer = new Timer;
-	//Setting variables for background
+	//Setting variables for background 
 	background.SetPosition(0, 0);
 	background.SetAngle(0.0);
 	background.SetSize(1280, 720);
-	//Setting Variables for Player
+	//Setting Variables for Player 
 	player.SetPosition(50, 400);
 	player.SetAngle(0.0);
 	player.SetSize(128, 128);
 	player.SetVelocity(2);
-	//Setting variables for cowboy
+	//Setting variables for cowboy 
 	cowboy.SetPosition(1150, 400);
 	cowboy.SetSize(128, 128);
 	cowboy.SetVelocity(1);
-	//Setting variables for Player's bullet
+	//Setting variables for Player's bullet 
 	bullet.SetSize(32, 32);
 	bullet.SetPosition(player.GetPosition());
-	//Initialising text objects
+	//Initialising text objects 
 	score->SetScore(0);
 	timer->SetTime(50);
 	while (m_isGameRunning)
@@ -52,7 +50,7 @@ Game::Game()
 		else if (timer->GetTime() >= 0)
 		{
 			timer->CountDown();
-			//========Updating position and/or Input translation============
+			//========Updating position and/or Input translation============ 
 			player.Update(input);
 			cowboy.Update(input);
 			if (player.IsBulletShot() && !bullet.IsActive())
@@ -65,7 +63,7 @@ Game::Game()
 			if (bullet.IsActive())
 			{
 				bullet.Update(input);
-				//=====Box Collision Detection=========
+				//=====Box Collision Detection========= 
 				BoxCollider cowBoyCollider = cowboy.GetCollider();
 				BoxCollider bulletCollider = bullet.GetCollider();
 				if (bulletCollider.IsColliding(cowBoyCollider))
@@ -85,8 +83,8 @@ Game::Game()
 				player.IsBulletShot(false);
 			}
 		}
-		//=====================================
-		//========rendering objects============
+		//===================================== 
+		//========rendering objects============ 
 		background.Render(screen);
 		player.Render(screen);
 		cowboy.Render(screen);
@@ -96,7 +94,6 @@ Game::Game()
 		}
 		score->Render(screen);
 		timer->Render(screen);
-
 		screen.Present();
 	}
 	delete score;
@@ -104,3 +101,4 @@ Game::Game()
 	Text::ShutDown();
 	screen.Exit();
 }
+Game::~Game() {}
---
+++ SDLGameAssignment/Game.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Screen.h"
+#include "Text.h"
+#include "Background.h"
+#include "Bullet.h"
+#include "CowboyP2.h"
+#include "Player.h"
+#include "score.h"
+#include "Timer.h"
+#include "Music.h"
+#include "Sound.h"
+class Game
+{
+public:
+	Game();
+	~Game();
+	bool m_isGameRunning;
+};
+
