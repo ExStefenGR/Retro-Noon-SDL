cf3d23d971a117b6d98ff89b637a9ae096ba6d13
Stefanos Triantafyllidis <stt0242@my.londonmet.ac.uk>
2021-10-06 22:38:29 +0100

    Further Cleanup before Version 1.2

--- SDLGameAssignment/Screen.cpp
+++ SDLGameAssignment/Screen.cpp
@@ -9,10 +9,10 @@ bool Screen::Init()
 	m_window = SDL_CreateWindow("Game that is fun a bit",
 								SDL_WINDOWPOS_CENTERED,	//X or Horizontal
 								SDL_WINDOWPOS_CENTERED, //Y or Vertical position
-								1280,		//Width
-								720,		//height
+								1920,		//Width
+								1080,		//height
 								0	);		//flags (fullscreen,focus,etc)
-	m_renderer = SDL_CreateRenderer(m_window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	m_renderer = SDL_CreateRenderer(m_window, -1, SDL_RENDERER_ACCELERATED);
 	return true;
 }
 void Screen::Present()
