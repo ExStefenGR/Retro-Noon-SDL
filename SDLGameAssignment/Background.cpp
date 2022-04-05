8f7dfad2cc6b4752e4c837cdb3689608e261d46a
Stefanos Triantafyllidis <stt0242@my.londonmet.ac.uk>
2022-04-05 16:11:37 +0100

    Upscale image to full HD

--- SDLGameAssignment/Background.cpp
+++ SDLGameAssignment/Background.cpp
@@ -4,7 +4,7 @@ Background::Background()
 {
 	m_images.Load("Assets/Images/Background.png");
 	m_images.SetImageDimension(1, 1, 1280, 720);
-	m_images.SetSpriteDimension(1280, 720);
+	m_images.SetSpriteDimension(1920, 1080);
 	m_images.IsAnimated(false);
 	m_images.IsAnimationLooping(false);
 	music.Load("Assets/Music/yes.wav");
